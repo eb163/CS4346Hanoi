b669aacb38e2d863e57a2ac1b943af19fccf7199
realEricBrun <ericbrun350@att.net>
2019-10-17 11:46:53 -0500

    implemented scoreTower function

--- CS4346Hanoi/CS4346Hanoi/scoreFunctions.cpp
+++ CS4346Hanoi/CS4346Hanoi/scoreFunctions.cpp
@@ -8,8 +8,18 @@ Implementations for the score function declarations in scoreFunctions.h
 
 int scoreTower(Tower t)
 {
-	//TODO
-	return -1;
+	int size = t.getSize();
+	int score = 0;
+	if (size > 0)
+	{
+		for (int index = 0; index < size; ++index)
+		{
+			short discVal = t.peekAt(index);
+			short weight = size - index; //an ideal score will have t ordered such that (size - index) == discVal
+			score += (discVal * weight);
+		}
+	}
+	return score;
 }
 
 int scoreGameState(GameState state)

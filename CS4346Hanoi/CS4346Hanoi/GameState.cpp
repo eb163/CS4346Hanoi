9fd32ec2d38cf6cd76ae617be012dba29538b5ac
realEricBrun <ericbrun350@att.net>
2019-10-17 13:45:44 -0500

    added Tower attributes and init method to GameState class

--- CS4346Hanoi/CS4346Hanoi/GameState.cpp
+++ CS4346Hanoi/CS4346Hanoi/GameState.cpp
@@ -2,9 +2,24 @@
 
 GameState::GameState()
 {
+	init(0);
+}
+
+GameState::GameState(short totalGameDiscs)
+{
+	this->init(totalGameDiscs);
 }
 
 
 GameState::~GameState()
 {
+}
+
+void GameState::init(short totalDiscs)
+{
+	tA.setName("A");
+	tB.setName("B");
+	tC.setName("C");
+
+	tA.init(totalDiscs);
 }
--- CS4346Hanoi/CS4346Hanoi/GameState.h
+++ CS4346Hanoi/CS4346Hanoi/GameState.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Tower.h"
 
 /*
 
@@ -8,8 +9,15 @@ A class for containing all three Tower states for the game.
 
 class GameState
 {
+private:
+	Tower tA;
+	Tower tB;
+	Tower tC;
 public:
 	GameState();
+	GameState(short totalGameDiscs);
 	~GameState();
+
+	void init(short totalDiscs);
 };
 

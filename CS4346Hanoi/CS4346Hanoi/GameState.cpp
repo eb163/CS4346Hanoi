87bc1b8188bede74f67bbcb13c22967fd2960fa9
realEricBrun <ericbrun350@att.net>
2019-10-18 12:31:52 -0500

    implemented a move() function with which the AI can interact with the GameState

--- CS4346Hanoi/CS4346Hanoi/GameState.cpp
+++ CS4346Hanoi/CS4346Hanoi/GameState.cpp
@@ -23,3 +23,76 @@ void GameState::init(short totalDiscs)
 
 	tA.init(totalDiscs);
 }
+
+Tower GameState::getTower(TowerSelection t)
+{
+	Tower tow;
+	if (t == TOWER_A)
+	{
+		tow = tA;
+	}
+	else if (t == TOWER_B)
+	{
+		tow = tB;
+	}
+	else if (t == TOWER_C)
+	{
+		tow = tC;
+	}
+	return tow;
+}
+
+bool GameState::move(TowerSelection start, TowerSelection goal)
+{
+	bool flag = false;
+
+	if (start != goal)
+	{
+		short disc = SHRT_MAX;
+		Tower startTower;
+		Tower goalTower;
+
+		//parse selections
+		switch (start)
+		{
+		case TOWER_A: startTower = tA; break;
+		case TOWER_B: startTower = tB; break;
+		case TOWER_C: startTower = tC; break;
+		}
+
+		switch (goal)
+		{
+		case TOWER_A: goalTower = tA; break;
+		case TOWER_B: goalTower = tB; break;
+		case TOWER_C: goalTower = tC; break;
+		}
+
+		//get top disc value from start tower
+		disc = startTower.peek();
+
+		//check if action would be valid on goal tower
+		flag = goalTower.isValidAction(disc);
+
+		if (flag == true) //take action if valid
+		{
+			disc = startTower.takeDisc();
+			goalTower.addDisc(disc);
+
+			//save changes
+			switch (start)
+			{
+			case TOWER_A: tA = startTower; break;
+			case TOWER_B: tB = startTower; break;
+			case TOWER_C: tC = startTower; break;
+			}
+			switch (goal)
+			{
+			case TOWER_A: tA = goalTower; break;
+			case TOWER_B: tB = goalTower; break;
+			case TOWER_C: tC = goalTower; break;
+			}
+		}
+	}
+
+	return flag; //return whether the action was completed or not
+}
--- CS4346Hanoi/CS4346Hanoi/GameState.h
+++ CS4346Hanoi/CS4346Hanoi/GameState.h
@@ -7,6 +7,8 @@ A class for containing all three Tower states for the game.
 
 */
 
+enum TowerSelection {TOWER_A = 0, TOWER_B = 1, TOWER_C = 2};
+
 class GameState
 {
 private:
@@ -19,5 +21,12 @@ public:
 	~GameState();
 
 	void init(short totalDiscs);
+
+	Tower getTower(TowerSelection tower); //returns a copy of the tower data structure
+
+	bool move(TowerSelection start, TowerSelection goal); 
+	//move the top disk from start to goal
+	//return true if the operation was valid
+	//or return false if the operation was invalid
 };
 

d7e9c31763292ba4fbdcc397fd32dd83e0241972
realEricBrun <ericbrun350@att.net>
2019-10-20 14:10:08 -0500

    added a priority queue implementation for holding open SearchNodes in A*

---
+++ CS4346Hanoi/CS4346Hanoi/PriorityQueue.cpp
@@ -0,0 +1,65 @@
+#include "PriorityQueue.h"
+
+SearchNode PriorityQueue::peek()
+{
+	return contents.front();
+}
+
+SearchNode PriorityQueue::pop()
+{
+	SearchNode front = contents.front();
+	contents.pop_front();
+	return front;
+}
+
+void PriorityQueue::add(SearchNode n)
+{
+	int fScore = n.getF();
+	if (isEmpty() == false)
+	{
+		list<SearchNode>::iterator index = contents.begin();
+		bool hitEnd = false;
+		bool inserted = false;
+		do
+		{
+			int currFScore = (*index).getF();
+			if (currFScore > fScore)
+			{
+				contents.insert(index, n); 
+				//inserts SearchNode n in the list just BEFORE the current element pointed to by index
+				inserted = true;
+			}
+			++index; //incr iterator
+			hitEnd = (index == contents.end());
+		} while (hitEnd == false && inserted == false);
+		if (hitEnd == true && inserted == false)
+		{//if the end of list is reached and no position was found, put n at back of list
+			contents.push_back(n);
+		}
+	}
+	else
+	{//if list is empty, just add n to list
+		contents.push_front(n);
+	}
+}
+
+int PriorityQueue::getSize()
+{
+	return contents.size();
+}
+
+bool PriorityQueue::isEmpty()
+{
+	return contents.empty();
+}
+
+PriorityQueue::PriorityQueue()
+{
+
+}
+
+
+PriorityQueue::~PriorityQueue()
+{
+
+}
---
+++ CS4346Hanoi/CS4346Hanoi/PriorityQueue.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "GameState.h"
+#include "SearchNode.h"
+#include <list> //used for the constant time insertion and deletion operations
+
+/*
+The PriorityQueue is a container for holding SearchNodes. 
+The PriorityQueue uses the F values of Nodes as a key to order by.
+The SearchNode with the lowest F value is located at the front.
+*/
+
+using std::list;
+
+class PriorityQueue
+{
+private:
+	list<SearchNode> contents;
+protected:
+
+public:
+	SearchNode peek();
+	SearchNode pop();
+	void add(SearchNode n);
+	int getSize();
+	bool isEmpty();
+	PriorityQueue();
+	~PriorityQueue();
+};
+
